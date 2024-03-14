ca422c488ef74c45a18b11fc45741bdbd2cad595
Brian <bikaloryan@gmail.com>
2024-03-15 02:31:18 +0300

    2-args correct output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,11 @@
 import sys
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
-    print("{} arguments".format(num_args), end="")
     if num_args == 0:
-        print(".")
+        print("0 arguments.")
+    elif num_args == 1:
+        print("{} argument:".format(num_args))
     else:
-        print(":")
+        print("{} arguments:".format(num_args))
     for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
