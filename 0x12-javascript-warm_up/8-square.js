8b3731c7746783ffaa9c5f5f76583c00d92e9773
Brian Kyalo <bikaloryan@gmail.com>
2025-09-14 11:59:10 +0300

    8-square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,12 +2,11 @@
 
 const args = process.argv.slice(2);
 const size = parseInt(args[0]);
-const char = args[1] || 'x';
 
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    console.log(char.repeat(size));
+    console.log('x'.repeat(size));
   }
 }
