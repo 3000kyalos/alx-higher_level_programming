5eee9a6937b1eea70b89c2bf076fa9d3ad52bca3
Brian Kyalo <bikaloryan@gmail.com>
2025-10-28 14:16:52 +0300

    0-readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+const path = process.argv[2];
+
+if (!path) {
+    console.error('Please provide a file path as the first argument.');
+    process.exit(1);
+}
+
+fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
