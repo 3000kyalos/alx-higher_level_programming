e769c9baecfb0e74bef4927f49692f0f874eaf9d
Brian Kyalo <bikaloryan@gmail.com>
2025-10-28 14:44:21 +0300

    0-readme semistandard edit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,14 +4,14 @@ const fs = require('fs')
 const path = process.argv[2];
 
 if (!path) {
-    console.error('Please provide a file path as the first argument.');
-    process.exit(1);
+  console.error('Please provide a file path as the first argument.');
+  process.exit(1);
 }
 
 fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+      console.error(err);
+      return;
+  }
+  console.log(data);
 });
